6c01a8120d8b91c02a4c15bbb18779a2888e539f
Oaim79 <zhangmiao612@gmail.com>
2026-04-06 12:49:11 +0800

    Remove authentication gate - public access

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import { GenerationProvider, useGeneration } from './context/GenerationContext';
-import AuthGate from './components/AuthGate';
+import { GenerationProvider } from './context/GenerationContext';
 import Header from './components/Header';
 import GenerationList from './components/GenerationList';
-
-function AppContent() {
-  const { isAuthenticated } = useGeneration();
-
-  if (!isAuthenticated) {
-    return <AuthGate />;
-  }
-
-  return (
-    <div className="min-h-screen bg-zinc-950">
-      <Header />
-      <main>
-        <GenerationList />
-      </main>
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <GenerationProvider>
-      <AppContent />
+      <div className="min-h-screen bg-zinc-950">
+        <Header />
+        <main>
+          <GenerationList />
+        </main>
+      </div>
     </GenerationProvider>
   );
 }
